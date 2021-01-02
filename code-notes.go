73edff8c9ccd6b65df96d925a3276415b80c5517
byan1197 <Bondyan97@gmail.com>
2021-01-02 11:53:26 -0500

    started on markdown initiation

--- code-notes.go
+++ code-notes.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	// "os/exec"
+	// "runtime"
 )
 
 // should extract into modules later on
-
 func createEntry(arg string) {
 	return
 }
 
-func setup(arg string) {
+func codeNoteInit(arg string) {
 	// 1. ask for directory
 	// 2. ask for name
-	return
-}
+	directory := "~/Documents/code-notes"
+	dirScan := "~/"
+	var name string
+	fmt.Printf("Enter a directory for your code diary. (Default %s)\n", directory)
+	fmt.Scanln(&dirScan)
+	if dirScan != "" {
+		directory = dirScan
+	}
+	fmt.Printf("Got it. You want your notes to be stored in %s", directory)
+	os.MkdirAll(directory, os.ModePerm)
 
-func edit(arg string) {
-	// 1. if no args, edit main page (kind of like a table of contents)
-	// 2. if args, then edit the specific section
+	fmt.Println("Enter your name")
+	fmt.Scanln(&name)
 	return
 }
 
@@ -30,35 +40,63 @@ func edit(arg string) {
 }
 
 func open(arg string) {
-	var err error
+	// var err error
 
-	switch runtime.GOOS {
-		case "linux":
-			err = exec.Command("xdg-open", url).Start()
-		case "windows":
-			err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-		case "darwin":
-			err = exec.Command("open", url).Start()
-		default:
-			err = fmt.Errorf("unsupported platform")
-	}
-	
-	if err != nil {
-		fmt.Errorf(err)
-	}
+	// switch runtime.GOOS {
+	// case "linux":
+	// 	err = exec.Command("xdg-open", url).Start()
+	// case "windows":
+	// 	err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	// case "darwin":
+	// 	err = exec.Command("open", url).Start()
+	// default:
+	// 	err = fmt.Errorf("unsupported platform")
+	// }
+
+	// if err != nil {
+	// 	fmt.Errorf(err)
+	// }
 
 }
 
-func main() {
-	
-	const commandsToModule = map[string][]interface{
-		"setup": setup,
-		"create-entry": createEntry,
-		"edit": edit,
-		"open": open
+type codeNoteConfiguration struct {
+	notesDirectory string `json:"notesDirectory"`
+	name           string `json:"name"`
+}
 
-	}
+func getConfig() codeNoteConfiguration {
+	file, _ := ioutil.ReadFile("~/.config-helper.json")
+
+	configContents := codeNoteConfiguration{}
+	_ = json.Unmarshal([]byte(file), &configContents)
+
+	return configContents
+}
+
+func main() {
+
+	commandsToModule := map[string]interface{}{
+		"setup":        codeNoteInit,
+		"create-entry": createEntry,
+		"edit":         edit,
+		"open":         open,
+		"help":         open}
+
 	args := os.Args
 
-	fmt.Println(args)
+	configuration := getConfig()
+
+	if configuration == (codeNoteConfiguration{}) && args[1] != "setup" {
+		fmt.Println("No configuration file. Please run \n\n\tcode-notes setup")
+		return
+	}
+
+	fn, foundFunction := commandsToModule[args[1]].(func(string))
+
+	if foundFunction {
+		fn("asdf")
+		return
+	}
+
+	fmt.Printf("%s is not a valid command command. See 'code-notes help'", args[1])
 }
